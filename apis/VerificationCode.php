03eca2c12e46dc2539935d9ec6d612bcb6e66e30
ericknkunda <macnkunda@gmail.com>
2022-12-14 02:21:13 +0200

    message to commit

---
+++ apis/VerificationCode.php
@@ -0,0 +1,11 @@
+<?php
+require_once("../../finalproject/db_connection.php");
+
+//selecting the last verification code
+$selectLastVerCode =$conn->prepare("select code from tbl_user where registration_id =(select max(registration_id) from tbl_user) and code_status =1");
+$selectLastVerCode->execute();
+while($fetch =$selectLastVerCode->fetch()){
+    $jsonArray[]= array("Code"=>$fetch['code']);
+}
+echo json_encode($jsonArray);
+?>
